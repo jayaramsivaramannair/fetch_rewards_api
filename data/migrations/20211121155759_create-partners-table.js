7368d95fabd1e2c62bac73ca7e3d4cff2b4364d3
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-11-21 16:59:05 -0500

    Renamed the columns in partners table to payer-id and payer

--- data/migrations/20211121155759_create-partners-table.js
+++ data/migrations/20211121155759_create-partners-table.js
@@ -1,11 +1,11 @@
 
 exports.up = async function(knex) {
-  await knex.schema.createTable('partners', tbl => {
-    tbl.increments('partner-id');
-    tbl.string('partner-name').unique().notNullable();
+  await knex.schema.createTable('payers', tbl => {
+    tbl.increments('payer-id');
+    tbl.string('payer').unique().notNullable();
   })
 };
 
 exports.down = async function(knex) {
-  await knex.schema.dropTableIfExists('partners');
+  await knex.schema.dropTableIfExists('payers');
 };
--- data/migrations/20211121160132_create-rewards-table.js
+++ data/migrations/20211121160132_create-rewards-table.js
@@ -5,13 +5,13 @@ exports.up = async function(knex) {
     tbl.integer('points').notNullable();
     tbl.timestamp('timestamp').notNullable().defaultTo(knex.fn.now());
     tbl.integer('user-id').references('user-id').inTable('users').notNullable().onDelete('CASCADE').onUpdate('CASCADE');
-    tbl.integer('partner-id').references('partner-id').inTable('partners').notNullable().onDelete('CASCADE').onUpdate('CASCADE');
+    tbl.integer('payer-id').references('payer-id').inTable('payers').notNullable().onDelete('CASCADE').onUpdate('CASCADE');
   })
 };
 
 exports.down = async function(knex) {
   //Since the rewards table has references to users and partners, we need to drop these tables first before drop rewards table
   await knex.schema.dropTableIfExists('users');
-  await knex.schema.dropTableIfExists('partners');
+  await knex.schema.dropTableIfExists('payers');
   await knex.schema.dropTableIfExists('rewards');
 };
